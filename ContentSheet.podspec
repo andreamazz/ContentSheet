ac19bfa03c50f1c02826109797b6373f6e8f7a2c
Andrea Mazzini <andrea.mazzini@gmail.com>
2018-10-24 14:47:51 +0200

    Siwft 4.2 support

--- ContentSheet.podspec
+++ ContentSheet.podspec
@@ -1,52 +1,30 @@
 
 Pod::Spec.new do |s|
 
-# ―――  Spec Metadata  ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
+  s.name         = "ContentSheet"
+  s.version      = "0.1.5"
+  s.summary      = "A simple control that enables presenting any view controller or any other object that can provide a view like an ActionSheet"
 
-s.name         = "ContentSheet"
-s.version      = "0.1.5"
-s.summary      = "A simple control that enables presenting any view controller or any other object that can provide a view like an ActionSheet"
+  s.description  = <<-DESC
+  ContentSheet enables presenting any content in an ActionSheet.
+  The content provider has to follow 'ContentSheetContentProtocol'.
+  Extensions for UIViewController and UINavigationController have been added in the pod itself so that comes right out of the box.
+  It is also possible to have the content expand or collapse based on collapsed height and expanded height.
+  If there is a scrollview and you'd like to expand or collapse the content seemlessly on scroll, that is also possible.
+  You could customize the background and whether the sheet should be dismissed on tapping outside content or not.
+  Callbacks are given for all the events as well. Both 'ContentSheetContentProtocol' and 'ContentSheetDelegate' get
+  callbacks for different events.
 
-s.description  = <<-DESC
-ContentSheet enables presenting any content in an ActionSheet.
-The content provider has to follow 'ContentSheetContentProtocol'.
-Extensions for UIViewController and UINavigationController have been added in the pod itself so that comes right out of the box.
-It is also possible to have the content expand or collapse based on collapsed height and expanded height.
-If there is a scrollview and you'd like to expand or collapse the content seemlessly on scroll, that is also possible.
-You could customize the background and whether the sheet should be dismissed on tapping outside content or not.
-Callbacks are given for all the events as well. Both 'ContentSheetContentProtocol' and 'ContentSheetDelegate' get
-callbacks for different events.
+  Checkout the example in the code.
+  DESC
 
-Checkout the example in the code.
-DESC
-
-s.homepage     = "https://github.com/flipkart-incubator/ContentSheet"
-
-
-# ―――  Spec License  ――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
-
-s.license      = { :type => "Apache License, Version 2.0", :file => "LICENSE" }
-
-
-# ――― Author Metadata  ――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
-
-s.author             = { "rajatgupta26" => "rajatkumargupta89@gmail.com" }
-
-
-# ――― Platform Specifics ――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
-
-s.platform     = :ios, "8.0"
-
-
-# ――― Source Location ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
-
-s.source       = { :git => "https://github.com/flipkart-incubator/ContentSheet.git", :tag => s.version.to_s }
-
-
-# ――― Source Code ―――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――― #
-
-s.source_files  = "ContentSheet/Classes", "ContentSheet/Classes/**/*.{h,m,swift}"
-s.exclude_files = "Classes/Exclude"
-
+  s.homepage     = "https://github.com/flipkart-incubator/ContentSheet"
+  s.license      = { :type => "Apache License, Version 2.0", :file => "LICENSE" }
+  s.author             = { "rajatgupta26" => "rajatkumargupta89@gmail.com" }
+  s.platform     = :ios, "8.0"
+  s.source       = { :git => "https://github.com/flipkart-incubator/ContentSheet.git", :tag => s.version.to_s }
+  s.source_files  = "ContentSheet/Classes", "ContentSheet/Classes/**/*.{h,m,swift}"
+  s.exclude_files = "Classes/Exclude"
+  s.swift_version = '4.2'
 
 end
--- Example/FKWidgetSheet/ViewController.swift
+++ Example/FKWidgetSheet/ViewController.swift
@@ -23,74 +23,74 @@ import ContentSheet
 
 
 class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    UINavigationBar.appearance().barTintColor = UIColor.purple
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  private var canPresent = false
+  var presentedController: SecondViewController?
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    canPresent = true
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        UINavigationBar.appearance().barTintColor = UIColor.purple
+    presentedController = SecondViewController(nibName: nil, bundle: nil)
+    presentedController?.willMove(toParent: self)
+    presentedController?.view.backgroundColor = UIColor.yellow
+    self.addChild(presentedController!)
+    var frame = self.view.bounds.insetBy(dx: 25, dy: 100)
+    frame.origin.y -= 50
+    presentedController?.view.frame = frame
+    self.view.addSubview(presentedController!.view)
+    presentedController?.didMove(toParent: self)
+    
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+    
+    if canPresent {
+      canPresent = false
+      
+      let secondVC = SecondViewController(nibName: nil, bundle: nil)
+      let navcon = UINavigationController(rootViewController: secondVC)
+      
+      self.present(inContentSheet: navcon, animated: true)
+    }
+  }
+  
+  @IBAction func presentCustomView(_ sender: Any) {
+    
+    let content: ContentSheetContentProtocol
+    
+    switch (sender as! UIButton).tag {
+    case 0:
+      let view = UIView()
+      view.backgroundColor = UIColor.cyan
+      view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+      content = view
+      break
+    case 1:
+      let customView = CustomView.customView()
+      content = customView
+      break
+    default:
+      let contentController = CustomObject()
+      content = contentController
+      break
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    private var canPresent = false
-    var a: SecondViewController?
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        canPresent = true
-        
-        a = SecondViewController(nibName: nil, bundle: nil)
-        a?.willMove(toParentViewController: self)
-        a?.view.backgroundColor = UIColor.yellow
-        self.addChildViewController(a!)
-        var frame = self.view.bounds.insetBy(dx: 25, dy: 100)
-        frame.origin.y -= 50
-        a?.view.frame = frame
-        self.view.addSubview(a!.view)
-        a?.didMove(toParentViewController: self)
-        
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesEnded(touches, with: event)
-        
-        if canPresent {
-            canPresent = false
-            
-            let secondVC = SecondViewController(nibName: nil, bundle: nil)
-            let navcon = UINavigationController(rootViewController: secondVC)
-            
-            self.present(inContentSheet: navcon, animated: true)
-        }
-    }
-    
-    @IBAction func presentCustomView(_ sender: Any) {
-        
-        let content: ContentSheetContentProtocol
-        
-        switch (sender as! UIButton).tag {
-        case 0:
-            let view = UIView()
-            view.backgroundColor = UIColor.cyan
-            view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-            content = view
-            break
-        case 1:
-            let customView = CustomView.customView()
-            content = customView
-            break
-        default:
-            let contentController = CustomObject()
-            content = contentController
-            break
-        }
-        
-        let contentSheet = ContentSheet(content: content)
-        contentSheet.showDefaultHeader = (sender as! UIButton).tag != 2
-        self.present(contentSheet, animated: true, completion: nil)
-    }
+    let contentSheet = ContentSheet(content: content)
+    contentSheet.showDefaultHeader = (sender as! UIButton).tag != 2
+    self.present(contentSheet, animated: true, completion: nil)
+  }
 }
 
